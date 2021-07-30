928ee4f5aa9d75c94c21c7c7ccbce31037cb2746
felipemartinezs <felipe.martinez.s@hotmail.com>
2021-07-29 19:04:19 -0700

    Inicializing Normalizing

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,5 @@
-#include <math.h>       /* atan2 */
 #include "kalman_filter.h"
+#include "iostream"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -10,12 +10,15 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+  x_ = x_in; // object state
+  P_ = P_in; // object covariance matrix
+  F_ = F_in; // state transition matrix
+  H_ = H_in; // measurement matrix
+  R_ = R_in; // measurement covariance matrix
+  Q_ = Q_in; // process covariance matrix
+
+  //VectorXd u;	// external motion
+  //MatrixXd I; // Identity matrix
 }
 
 void KalmanFilter::Predict() {
@@ -23,7 +26,7 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
-  x_ = F_ * x_;
+  x_ = F_ * x_ ; // x = Fx+u Where u<<0,0,0,0 because noise mean is 0;
   MatrixXd Ft = F_.transpose();
   P_ = F_ * P_ * Ft + Q_;
 }
@@ -32,24 +35,20 @@ void KalmanFilter::Update(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Kalman Filter equations
-    ===================================================
-    Lesson 24 - 7 Kalman Filtr Equatiom in C++ Part 1
-                23
-    =====================================================
   */
   VectorXd z_pred = H_ * x_;
-  VectorXd y = z - z_pred;
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd K = PHt * Si;
+	VectorXd y = z - z_pred;   // error
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
 
-  //new estimate
-  x_ = x_ + (K * y);
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -57,42 +56,50 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+   //map predicated state x_ into RADAR measurement space: rho, theta, rho_dot
+   //Formula is from Lecture L5.14
+   float px = x_(0);
+   float py = x_(1);
+   float vx = x_(2);
+   float vy = x_(3);
 
-  /* Normalizing the angle
-  ======================================================*/
- VectorXd y = z - h_;
-  // Check if angle is within -pi to pi, if not then make it between -pi to pi
-  if(y[1] < -M_PI){
-    y[1] += ((double)2 * M_PI);
-  }
-  if(y[1] > M_PI){
-      y[1] -= ((double)2 * M_PI);
-  }
+   // Cartesian to polar   
+   double rho = sqrt(px*px + py*py);
+   //double theta = atan(py / px);
+   double theta = atan2(py, px);  // In C++, atan2() returns values between -pi and pi
+   double rho_dot = (px*vx + py*vy) / rho;
+   VectorXd z_pred = VectorXd(3); // h(x_)
+   z_pred << rho, theta, rho_dot;
    
-  /*=========================================================*/
+   // Calculations are essentially the same to the Update function
+   VectorXd y = z - z_pred;  //error
 
+   /* Normalizing angle
+     You'll need to make sure to normalize ϕ in the y vector so that its angle is between −pi and pi; 
+     in other words, add or subtract 2pi from ϕ until it is between −pi and pi. */
+  const double pi = 3.14;  // 3.14159265358979323846
+  if (y(1) < -pi) {
+    std::cout << "---------------------------------------- Theta is < -3.14.  ϕ = " << y(1);
+    y(1) = y(1) + 2*pi;
+    std::cout << " ---------------------------------------- Normalized Theta ϕ = " << y(1) << std::endl;
 
-
-  float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
-  float phi = atan2(x_(1), x_(0));
-  float rho_dot;
-  if (fabs(rho) < 0.0001) {
-    rho_dot = 0;
-  } else {
-    rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
   }
-  VectorXd z_pred(3);
-  z_pred << rho, phi, rho_dot;
-  VectorXd y = z - z_pred;
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd K = PHt * Si;
+  else if (y(1) > pi) {
+    std::cout << "---------------------------------------- Theta is < -3.14.  ϕ = " << y(1);
+    y(1) = y(1) - 2*pi;
+    std::cout << " ---------------------------------------- Normalized Theta ϕ = " << y(1) << std::endl;
+  }
 
-  //new estimate
-  x_ = x_ + (K * y);
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
+
+   MatrixXd Ht = H_.transpose();
+   MatrixXd S = H_ * P_ * Ht + R_;
+   MatrixXd Si = S.inverse();
+   MatrixXd PHt = P_ * Ht;
+   MatrixXd K = PHt * Si;
+ 
+   //new estimate
+   x_ = x_ + (K * y);
+   long x_size = x_.size();
+   MatrixXd I = MatrixXd::Identity(x_size, x_size);
+   P_ = (I - K * H_) * P_;
 }
